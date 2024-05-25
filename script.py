2d176be020a544c52c38efd5af1ea48b9b72d624
ElenaGolubeva <elenagolubeva2003@mail.ru>
2024-05-25 14:35:09 +0300

    Create script.py

---
+++ script.py
@@ -0,0 +1,6 @@
+﻿import requests
+
+data = {"name": ["value1", "value2", "value3", "value4", "value5", "value6"]}
+
+response = requests.post("http://localhost:8000/data", json=data)
+print(response.json())
